f5137fb44d046a23799414f2464e49a59ac14512
Maxim Mochalkin <maksimmochalkin@gmail.com>
2021-10-02 19:28:06 +0600

    overrided Equals

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -11,5 +11,20 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public bool Equals(Product? product)
+        {
+            if (product == null)
+            {
+                return false;
+            }
+
+            if (this.Name == product.Name && this.Price == product.Price)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
